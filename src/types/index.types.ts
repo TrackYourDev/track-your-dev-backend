1b080a477d14a68ec694c78ea2146f52c5501116
Himanshu Saini <hs913271@gmail.com>
2025-06-01 16:05:54 +0530

    i have added api call to fetch file changes and code

--- src/types/index.types.ts
+++ src/types/index.types.ts
@@ -3,6 +3,113 @@ interface IFileChange {
   filePath: string;
   changeType: 'added' | 'removed' | 'modified';
 }
+
+interface IGitHubUser {
+  name?: string;
+  email?: string | null;
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+}
+
+interface IGitHubRepository {
+  id: number;
+  node_id: string;
+  name: string;
+  full_name: string;
+  private: boolean;
+  owner: IGitHubUser;
+  html_url: string;
+  description: string | null;
+  fork: boolean;
+  url: string;
+  created_at: number;
+  updated_at: string;
+  pushed_at: number;
+  git_url: string;
+  ssh_url: string;
+  clone_url: string;
+  default_branch: string;
+  organization?: string;
+}
+
+interface IGitHubOrganization {
+  login: string;
+  id: number;
+  node_id: string;
+  url: string;
+  repos_url: string;
+  events_url: string;
+  hooks_url: string;
+  issues_url: string;
+  members_url: string;
+  public_members_url: string;
+  avatar_url: string;
+  description: string;
+}
+
+interface IGitHubCommit {
+  id: string;
+  tree_id: string;
+  distinct: boolean;
+  message: string;
+  timestamp: string;
+  url: string;
+  author: {
+    name: string;
+    email: string;
+    username: string;
+  };
+  committer: {
+    name: string;
+    email: string;
+    username: string;
+  };
+  added: string[];
+  removed: string[];
+  modified: string[];
+}
+
+export interface GitHubWebhookPayload {
+  ref: string;
+  before: string;
+  after: string;
+  repository: IGitHubRepository;
+  pusher: {
+    name: string;
+    email: string;
+  };
+  organization: IGitHubOrganization;
+  sender: IGitHubUser;
+  installation: {
+    id: number;
+    node_id: string;
+  };
+  created: boolean;
+  deleted: boolean;
+  forced: boolean;
+  base_ref: string | null;
+  compare: string;
+  commits: IGitHubCommit[];
+  head_commit: IGitHubCommit;
+  pushed_at: number;
+}
+
 export interface ICommit {
   sha: string;
   message: string;
