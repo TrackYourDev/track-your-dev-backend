c1b26192aa385b0ef46290658e45cd322ef89e95
Himanshu Saini <hs913271@gmail.com>
2025-06-05 13:11:22 +0530

    updated private-key to be fetched from base64 env

--- src/utils/generateJWT.ts
+++ src/utils/generateJWT.ts
@@ -1,17 +1,23 @@
 import jwt from 'jsonwebtoken';
 import fs from 'fs';
 
-const appId = '1265874'; // from GitHub Developer App
+const appId = '1265874';
 
 let privateKey = process.env.PRIVATE_KEY;
 
-if (!privateKey) privateKey = fs.readFileSync('./private-key.pem', 'utf8');
+if (!privateKey && process.env.PRIVATE_KEY_BASE64) {
+  privateKey = Buffer.from(process.env.PRIVATE_KEY_BASE64, 'base64').toString('utf8');
+}
+
+if (!privateKey) {
+  privateKey = fs.readFileSync('./private-key.pem', 'utf8'); // fallback for local dev
+}
 
 const jwtToken = jwt.sign(
   {
     iss: appId,
     iat: Math.floor(Date.now() / 1000),
-    exp: Math.floor(Date.now() / 1000) + (10 * 60), // max 10 minutes
+    exp: Math.floor(Date.now() / 1000) + 10 * 60,
   },
   privateKey,
   { algorithm: 'RS256' }
