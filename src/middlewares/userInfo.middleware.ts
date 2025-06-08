1fa18066b16e7be06085feaa156ac4326a1974ad
Himanshu Saini <hs913271@gmail.com>
2025-06-08 15:43:43 +0530

    fixed middleware

--- src/middlewares/userInfo.middleware.ts
+++ src/middlewares/userInfo.middleware.ts
@@ -5,19 +5,34 @@ interface AuthRequest extends Request {
   githubId?: string;
 }
 
-export const authenticateToken = (req: AuthRequest, res: Response, next: NextFunction): void => {
-  const token = req.headers.authorization?.split(" ")[1];
+export const authenticateToken = async (req: AuthRequest, res: Response, next: NextFunction): Promise<void> => {
+  const token = req.cookies.get('github_token')?.value;
   if (!token) {
     res.status(401).json({ message: "No token provided" });
     return;
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as { githubId: string };
-    req.githubId = decoded.githubId;
-    next();
+    const response = await fetch('https://api.github.com/user', {
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Accept': 'application/vnd.github.v3+json',
+        'User-Agent': 'trackyourdev'
+      }
+    });
+
+
+    if (response.ok) {
+      const userData = await response.json();
+      console.log(userData);
+      req.githubId = userData.id;
+      next();
+    } else {
+      res.status(401).json({ message: "Invalid token" });
+      return;
+    }
   } catch (err) {
-     res.status(403).json({ message: "Invalid token" });
-     return
+    res.status(403).json({ message: "Invalid token" });
+    return
   }
 };
